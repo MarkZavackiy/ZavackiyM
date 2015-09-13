fb8bc44ffcbd5bc780e29f6e7ba73fd769a07c1c
Марк <sky.warior@mail.ru>
2015-09-13 14:08:22 +0300

    Initial commit

---
+++ src/com/google/ZavackiyM/lab1/FirstApp.java
@@ -0,0 +1,11 @@
+package com.google.ZavackiyM.lab1;
+
+/**
+ * Created by Марк on 13.09.2015.
+ */
+public class FirstApp {
+
+    public static void main(String[] args) {
+        System.out.println(1+2+3+4+5);
+    }
+}
